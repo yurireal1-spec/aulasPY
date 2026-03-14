2ce896aeda046c87f77d706db4f61be44758c1bf
yuri <yurireal1@gmail.om>
2026-03-13 21:33:50 -0300

    Tabuada

--- Aula 5/tabuada_de_um_numero.py
+++ Aula 5/tabuada_de_um_numero.py
@@ -1,6 +1,10 @@
+import os, time
 #Gerando números com for 
 Valor=int(input("Digite um Valor: "))
 
 print(f"Tabuada do {Valor}")
 for i in range(1,11):
     print(f"{Valor} x {i} = {Valor*i}")
+
+time.sleep(6)
+os.system("cls")
